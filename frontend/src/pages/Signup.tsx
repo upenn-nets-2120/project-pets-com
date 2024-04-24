844a91d56c93889090363f2d981662e06338c4d3
Owen Lester <owlester@sas.upenn.edu>
2024-04-24 02:38:19 -0400

    removed linked_nconst

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,105 +1,103 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios'; 
-import config from '../../config.json';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import config from "../../config.json";
 
 export default function Signup() {
-    const navigate = useNavigate(); 
-    
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [linked_id, setLinkedNconst] = useState('');
+  const navigate = useNavigate();
 
-    // TODO: set appropriate state variables 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const rootURL = config.serverRootURL;
+  // TODO: set appropriate state variables
 
-    const handleSubmit = async () => {
-        // TODO: make sure passwords match
+  const rootURL = config.serverRootURL;
 
-        if (password !== confirmPassword) {
-            console.log("Passwords don't match!")
-            alert("Registration failed.");
-            return;
-        }
+  const handleSubmit = async () => {
+    // TODO: make sure passwords match
 
-        // TODO: send registration request to backend
-        try {
-            const response = await axios.post(`${rootURL}/register`, {
-                username,
-                password,
-                linked_id
-            });
+    if (password !== confirmPassword) {
+      console.log("Passwords don't match!");
+      alert("Registration failed.");
+      return;
+    }
 
-            if (response.status === 200) {
-                navigate("/"+ username+"/home");
-            } else {
-                alert('Registration failed.');
-            }
-        } catch (error) {
-            console.log(error); 
-            alert('Registration failed.');
-        }                
-    };
+    // TODO: send registration request to backend
+    try {
+      const response = await axios.post(`${rootURL}/register`, {
+        username,
+        password,
+      });
 
-    return (
-        <div className='w-screen h-screen flex items-center justify-center'>
-            <form onSubmit={handleSubmit}>
-                <div className='rounded-md bg-slate-50 p-6 space-y-2 w-full'>
-                    <div className='font-bold flex w-full justify-center text-2xl mb-4'>
-                        Sign Up to Pennstagram
-                    </div>
-                    <div className='flex space-x-4 items-center justify-between'>
-                        <label htmlFor="username" className='font-semibold'>Username</label>
-                        <input
-                            id="username"
-                            type="text"
-                            className='outline-none bg-white rounded-md border border-slate-100 p-2'
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className='flex space-x-4 items-center justify-between'>
-                        <label htmlFor="linked_nconst" className='font-semibold'>Linked nconst</label>
-                        <input
-                            id="linked_nconst"
-                            type="text"
-                            className='outline-none bg-white rounded-md border border-slate-100 p-2'
-                            value={linked_id}
-                            onChange={(e) => setLinkedNconst(e.target.value)}
-                        />
-                    </div>
-                    <div className='flex space-x-4 items-center justify-between'>
-                        <label htmlFor="password" className='font-semibold'>Password</label>
-                        <input
-                            id="password"
-                            type="password"
-                            className='outline-none bg-white rounded-md border border-slate-100 p-2'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className='flex space-x-4 items-center justify-between'>
-                        <label htmlFor="confirmPassword" className='font-semibold'>Confirm Password</label>
-                        <input
-                            id="confirmPassword"
-                            type="password"
-                            className='outline-none bg-white rounded-md border border-slate-100 p-2'
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className='w-full flex justify-center'>
-                        <button onClick={(e) => {e.preventDefault() ;handleSubmit()}}
-                            type="submit"
-                            className='px-4 py-2 rounded-md bg-indigo-500 outline-none font-bold text-white'
-                        >
-                            Sign up
-                        </button>
-                    </div>
-                </div>
-            </form>
+      if (response.status === 200) {
+        navigate("/" + username + "/home");
+      } else {
+        alert("Registration failed.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Registration failed.");
+    }
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit}>
+        <div className="rounded-md bg-slate-50 p-6 space-y-2 w-full">
+          <div className="font-bold flex w-full justify-center text-2xl mb-4">
+            Sign Up to Pennstagram
+          </div>
+          <div className="flex space-x-4 items-center justify-between">
+            <label htmlFor="username" className="font-semibold">
+              Username
+            </label>
+            <input
+              id="username"
+              type="text"
+              className="outline-none bg-white rounded-md border border-slate-100 p-2"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="flex space-x-4 items-center justify-between">
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="outline-none bg-white rounded-md border border-slate-100 p-2"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex space-x-4 items-center justify-between">
+            <label htmlFor="confirmPassword" className="font-semibold">
+              Confirm Password
+            </label>
+            <input
+              id="confirmPassword"
+              type="password"
+              className="outline-none bg-white rounded-md border border-slate-100 p-2"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <div className="w-full flex justify-center">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                handleSubmit();
+              }}
+              type="submit"
+              className="px-4 py-2 rounded-md bg-indigo-500 outline-none font-bold text-white"
+            >
+              Sign up
+            </button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
